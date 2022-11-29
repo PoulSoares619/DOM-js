a334a2d1c35140d3a6cf2eb5884202a29a6420a3
Poul <poul.pheez2@gmail.com>
2022-11-29 12:55:37 -0300

    hoisting

--- robotron-2000-projeto_inicial/js/main.js
+++ robotron-2000-projeto_inicial/js/main.js
@@ -1,6 +1,16 @@
-const robotron = document.querySelector("#robotronn");
+const somar = document.querySelector("#somar");
+const subtrair = document.querySelector("#subtrair");
+const bracos = document.querySelector("#bracos");
 
-robotron.addEventListener("click", () =>{
-    console.log('fui clikado');
-});
+somar.addEventListener("click", () =>{manipulaDados("somar")});
+
+subtrair.addEventListener("click", () =>{manipulaDados("subtrair")});
+
+function manipulaDados(operacao) {
+    if(operacao === "subtrair"){
+        bracos.value = parseInt(bracos.value) - 1;
+    } else{
+        bracos.value = parseInt(bracos.value) + 1;
+    }
+}
 
