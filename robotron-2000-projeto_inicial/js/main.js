874cfce61fcce9fa229f6500a1ba625a64db1720
Poul <poul.pheez2@gmail.com>
2022-11-30 13:52:23 -0300

    dataset

--- robotron-2000-projeto_inicial/js/main.js
+++ robotron-2000-projeto_inicial/js/main.js
@@ -1,14 +1,14 @@
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode);
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
     })
 })
 
 
 function manipulaDados(operacao, controle) {
-    const pecas = controle.querySelector(".controle-contador");
+    const pecas = controle.querySelector("[data-contador]");
 
     if(operacao === "-"){
         pecas.value = parseInt(pecas.value) - 1;
